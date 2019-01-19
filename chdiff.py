cf1f8f96ef7d973d4ea15ca4d28688319cb23761
Sören Köhler <soerenkoehler@users.noreply.github.com>
2019-01-19 22:48:44 +0100

    Build 2019-01-19

--- chdiff.py
+++ chdiff.py
@@ -19,7 +19,7 @@ ChDiff - a checksum based diff and backup tool
 EPILOG = """
 -----------------------------------------------
 https://github.com/soerenkoehler/checksum-diff/
-Build 2019-01-03 01:11:00 (Backport Python 3.5)
+Build 2019-01-19 22:41:55 (Backport Python 3.5)
 (requires Python 3.5)
 """
 
